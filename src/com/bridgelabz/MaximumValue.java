c1a9a8219ec3c7138ca96d262c030f77112f8c2c
mehulbhange <bhangemehul@gmail.com>
2022-08-26 14:16:11 +0530

    ADDED:[Mehul] added welcome message

---
+++ src/com/bridgelabz/MaximumValue.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To find maximum value using generics
+ * @author - Mehul Bhange
+ * @since - 26-08-2022
+ */
+public class MaximumValue {
+    public static void main(String[] args) {
+        System.out.println("Find Maximum Problem using Generics");
+
+    }
+}
