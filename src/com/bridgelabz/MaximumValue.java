a577bc94561e47e988836f4d8dc81da3c2de0282
mehulbhange <bhangemehul@gmail.com>
2022-08-26 14:43:40 +0530

    ADDED:[Mehul] added method to find maximum float value

--- src/com/bridgelabz/MaximumValue.java
+++ src/com/bridgelabz/MaximumValue.java
@@ -18,9 +18,24 @@ public class MaximumValue {
 
         return  max;
     }
+    /*
+     * This method used to compare three float values and get the maximum value
+     * It returns maximum float value
+     * */
+    public static Float findMaxFloat(Float num1, Float num2, Float num3){
+        Float max = num1;
+        if (num2.compareTo(max) >= 0)
+            max = num2;
+        if (num3.compareTo(max) >= 0)
+            max = num3;
+
+        return  max;
+    }
     public static void main(String[] args) {
         System.out.println("Find Maximum Problem using Generics");
         //called findMaxInteger() 
         System.out.println("The maximum value between the three integer is : " + findMaxInteger(13, 8, 15));
+
+        System.out.println("The maximum value between the three float is : " + findMaxFloat(2.5f, 4.5f, 0.5f));
     }
 }
