bbe9e1615872bcb1efd9edfe8fbd34526624a7f2
mehulbhange <bhangemehul@gmail.com>
2022-08-26 15:14:55 +0530

    REFACTORED:[Mehul] refactored all 3 methods to 1 generic method

--- src/com/bridgelabz/MaximumValue.java
+++ src/com/bridgelabz/MaximumValue.java
@@ -5,37 +5,12 @@ package com.bridgelabz;
  * @since - 26-08-2022
  */
 public class MaximumValue {
+
     /*
-    * This method used to compare three integer values and get the maximum value
-    * It returns maximum integer value
+    * Generic method to compare any type of data and find maximum
     * */
-    public static Integer findMaxInteger(Integer num1, Integer num2, Integer num3){
-        Integer max = num1;
-        if (num2.compareTo(max) >= 0)
-            max = num2;
-        if (num3.compareTo(max) >= 0)
-            max = num3;
-
-        return  max;
-    }
-    /*
-     * This method used to compare three float values and get the maximum value
-     * It returns maximum float value
-     * */
-    public static Float findMaxFloat(Float num1, Float num2, Float num3){
-        Float max = num1;
-        if (num2.compareTo(max) >= 0)
-            max = num2;
-        if (num3.compareTo(max) >= 0)
-            max = num3;
-
-        return  max;
-    }
-    /*
-     * This method used to compare three String values and get the maximum value
-     * */
-    public static String findMaxString(String val1, String val2, String val3){
-        String max = val1;
+    public static <T extends Comparable<T>> T findMaximum(T val1, T val2, T val3){
+        T max = val1;
         if (val2.compareTo(max) >= 0)
             max = val2;
         if (val3.compareTo(max) >= 0)
@@ -45,11 +20,11 @@ public class MaximumValue {
     }
     public static void main(String[] args) {
         System.out.println("Find Maximum Problem using Generics");
-        //called findMaxInteger() 
-        System.out.println("The maximum value between the three integer is : " + findMaxInteger(13, 8, 15));
 
-        System.out.println("The maximum value between the three float is : " + findMaxFloat(2.5f, 4.5f, 0.5f));
+        System.out.println("The maximum value between the three integer is : " + findMaximum(13, 8, 15));
 
-        System.out.println("The maximum value between the three string is : " + findMaxString("Apple", "Peach", "Banana"));
+        System.out.println("The maximum value between the three float is : " + findMaximum(2.5f, 4.5f, 0.5f));
+
+        System.out.println("The maximum value between the three string is : " + findMaximum("Apple", "Peach", "Banana"));
     }
 }
